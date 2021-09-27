a8fbb8fd35d5074dd2272d64b2134d6f5b9e2306
dennis.jensen03 <dennis.jensen03@gmail.com>
2021-09-27 06:57:13 -0400

    had to include the while statement

--- simpletest.py
+++ simpletest.py
@@ -7,10 +7,11 @@ led_pin = 4
 GPIO.setup(led_pin, GPIO.OUT)
 try:
     print("turning on LED pin")
-    GPIO.output(led_pin, True)
-    # time.sleep(0.5)
-    # GPIO.output(led_pin, False)
-    # time.sleep(0.5)
+    while True:
+        GPIO.output(led_pin, True)
+        # time.sleep(0.5)
+        # GPIO.output(led_pin, False)
+        # time.sleep(0.5)
 finally:
     print("Cleaning up")
     GPIO.cleanup()
