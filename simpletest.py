67249900aa424ec7a6e653f02a365b07812efd36
dennis.jensen03 <dennis.jensen03@gmail.com>
2021-09-27 06:41:10 -0400

    a simple script that turn on pin 4

---
+++ simpletest.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+
+import time
+
+GPIO.setmode(GPIO.BCM)
+led_pin = 4
+GPIO.setup(led_pin, GPIO.OUT)
+try:
+    GPIO.output(led_pin, True)
+    # time.sleep(0.5)
+    # GPIO.output(led_pin, False)
+    # time.sleep(0.5)
+finally:
+    print("Cleaning up")
+    GPIO.cleanup()
